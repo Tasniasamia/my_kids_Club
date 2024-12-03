1bdd7738c6ce1fec6e2677041dc281b7830455d8
tasniaarhamarham <tasniaarhamarham@gmail.com>
2024-12-03 11:23:48 +0600

    first

---
+++ app/components/layout/header.js
@@ -0,0 +1,288 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { usePathname } from "next/navigation";
+import { Badge, Dropdown, Space } from "antd";
+import { FaRegUser, FaRegHeart, FaSearch, FaBars } from "react-icons/fa";
+import { DownOutlined, SmileOutlined } from "@ant-design/icons";
+import { CiSearch, CiHeart, CiUser } from "react-icons/ci";
+import { BiShoppingBag } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
+
+const Header = () => {
+  const pathname = usePathname();
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const items = [
+    {
+      key: "1",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.antgroup.com"
+        >
+          1st menu item
+        </a>
+      ),
+    },
+    {
+      key: "2",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.aliyun.com"
+        >
+          2nd menu item (disabled)
+        </a>
+      ),
+      icon: <SmileOutlined />,
+      disabled: true,
+    },
+    {
+      key: "3",
+      label: (
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.luohanacademy.com"
+        >
+          3rd menu item (disabled)
+        </a>
+      ),
+      disabled: true,
+    },
+    {
+      key: "4",
+      danger: true,
+      label: "a danger item",
+    },
+  ];
+  return (
+    <div className="bg-cyan py-[10px]">
+      <div className="container ">
+        <div className="flex justify-between items-center ">
+          <Image
+            src="/logo.png"
+            width={136.69}
+            height={70}
+            alt="logo"
+            className="md:h-[70px] md:w-[136.69px] w-[100px] h-[40px] object-cover cursor-pointer"
+            href="/"
+          />
+          <div className="flex items-center xl:gap-[65.06px] gap-[20px]">
+            <ul className="list-none lg:flex hidden xl:gap-[40px] gap-[20px] items-center">
+              <li>
+                <Link
+                  href="/"
+                  className={`${
+                    pathname === '/"' ? "text-primary" : "text-textprimary"
+                  } text-[18px] hover:text-primary duration-300 transition-all text-textprimary font-semibold leading-[26px] cursoer-pointer  capitalize`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/about"
+                  className={`${
+                    pathname === '/"' ? "text-primary" : "text-textprimary"
+                  } hover:text-primary duration-300 transition-all  text-[18px] text-textprimary font-semibold leading-[26px] cursoer-pointer capitalize`}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/service"
+                  className={`${
+                    pathname === '/"' ? "text-primary" : "text-textprimary"
+                  } hover:text-primary duration-300 transition-all  text-[18px] text-textprimary font-semibold leading-[26px] cursoer-pointer  capitalize`}
+                >
+                  Service
+                </Link>
+              </li>
+              <li>
+                <Dropdown
+                  menu={{
+                    items,
+                  }}
+                >
+                  <a
+                    onClick={(e) => e.preventDefault()}
+                    className="hover:text-primary duration-300 transition-all   text-textprimary  cursoer-pointer flex gap-1 items-center capitalize"
+                  >
+                    <span className="font-semibold leading-[26px] text-[18px] ">
+                      Pages +
+                    </span>
+                  </a>
+                </Dropdown>
+              </li>
+              <li>
+                <Link
+                  href="/blog"
+                  className={`${
+                    pathname === '/"' ? "text-primary" : "text-textprimary"
+                  } hover:text-primary duration-300 transition-all  text-[18px] text-textprimary font-semibold leading-[26px] cursoer-pointer capitalize`}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/pricing"
+                  className={`${
+                    pathname === '/"' ? "text-primary" : "text-textprimary"
+                  } hover:text-primary duration-300 transition-all  text-[18px] text-textprimary font-semibold leading-[26px] cursoer-pointer  capitalize`}
+                >
+                  Pricing
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/contact"
+                  className={`${
+                    pathname === '/"' ? "text-primary" : "text-textprimary"
+                  } hover:text-primary duration-300 transition-all  text-[18px] text-textprimary font-semibold leading-[26px] cursoer-pointer  capitalize`}
+                >
+                  contact
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/cart"
+                  className={`${
+                    pathname === '/"' ? "text-primary" : "text-textprimary"
+                  } hover:text-primary duration-300 transition-all  text-[18px] text-textprimary font-semibold leading-[26px] cursoer-pointer capitalize`}
+                >
+                  cart(0)
+                </Link>
+              </li>
+            </ul>
+            <ul className="list-none flex gap-[10px] items-center">
+              <li className="bg-textprimary sm:w-[35px] sm:h-[35px] w-[28px] h-[28px] rounded-full text white flex justify-center items-center text-white font-bold">
+                <FaSearch size={14.16} />
+              </li>
+              <li className="bg-textprimary sm:w-[35px] sm:h-[35px] w-[28px] h-[28px] rounded-full text white flex justify-center items-center text-white font-bold">
+                <FaRegHeart size={14.16} />
+              </li>
+
+              <Badge count={0} showZero>
+                <li className="bg-textprimary sm:w-[35px] sm:h-[35px] w-[28px] h-[28px] rounded-full text white flex justify-center items-center text-white font-bold">
+                  <BiShoppingBag size={14.16} />
+                </li>
+              </Badge>
+
+              <li className="bg-textprimary sm:w-[35px] sm:h-[35px] w-[28px] h-[28px] rounded-full text white flex justify-center items-center text-white font-bold text-[14.16px]">
+                <FaRegUser />
+              </li>
+              <li
+                className="bg-textprimary sm:w-[35px] sm:h-[35px] w-[28px] h-[28px] rounded-full lg:hidden  flex justify-center items-center text-white font-bold text-[14.16px] "
+                onClick={handleToggle}
+              >
+                {toggle ? <IoClose /> : <FaBars />}
+              </li>
+            </ul>
+          </div>
+          {toggle && (
+            <div className="absolute top-[130px] left-0 w-full bg-cyan z-50">
+              <ul className="list-none flex flex-col gap-[10px] items-center p-4">
+                <li>
+                  <Link
+                    href="/"
+                    className={`${
+                      pathname === "/" ? "text-primary" : "text-textprimary"
+                    } hover:text-primary duration-300 transition-all text-[18px] font-semibold capitalize`}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/about"
+                    className={`${
+                      pathname === "/about"
+                        ? "text-primary"
+                        : "text-textprimary"
+                    } hover:text-primary duration-300 transition-all text-[18px] font-semibold capitalize`}
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/service"
+                    className={`${
+                      pathname === "/service"
+                        ? "text-primary"
+                        : "text-textprimary"
+                    } hover:text-primary duration-300 transition-all text-[18px] font-semibold capitalize`}
+                  >
+                    Service
+                  </Link>
+                </li>
+                <li>
+                <Dropdown
+                  menu={{
+                    items,
+                  }}
+                >
+                  <a
+                    onClick={(e) => e.preventDefault()}
+                    className="hover:text-primary duration-300 transition-all   text-textprimary  cursoer-pointer flex gap-1 items-center capitalize"
+                  >
+                    <span className="font-semibold leading-[26px] text-[18px] ">
+                      Pages +
+                    </span>
+                  </a>
+                </Dropdown>
+              </li>
+                <li>
+                  <Link
+                    href="/blog"
+                    className={`${
+                      pathname === "/blog" ? "text-primary" : "text-textprimary"
+                    } hover:text-primary duration-300 transition-all text-[18px] font-semibold capitalize`}
+                  >
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/pricing"
+                    className={`${
+                      pathname === "/pricing"
+                        ? "text-primary"
+                        : "text-textprimary"
+                    } hover:text-primary duration-300 transition-all text-[18px] font-semibold capitalize`}
+                  >
+                    Pricing
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/contact"
+                    className={`${
+                      pathname === "/contact"
+                        ? "text-primary"
+                        : "text-textprimary"
+                    } hover:text-primary duration-300 transition-all text-[18px] font-semibold capitalize`}
+                  >
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/components/layout/topbar.js
@@ -0,0 +1,69 @@
+"use client";
+import { MdOutlineEmail } from "react-icons/md";
+import {
+  FaPhoneAlt,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa6";
+
+const Topbar = () => {
+  return (
+    <div className="bg-cyan py-3 border-b border-white">
+        <div className="container mx-auto">
+      <div className=" w-full flex  justify-between items-center md:gap-0 gap-[10px] sm:flex-row flex-col">
+        <ul className="list-none flex lg:gap-[40px] sm:gap-[10px]  gap-[20px] items-center">
+          <li className="flex items-center ">
+            <MdOutlineEmail  className="text-white me-[4.57px] sm:text-[15.71px] text-[12px]" />
+            <span className="text-toptext text-xs font-bold sm:me-[5px] sm:block hidden">
+              Email Us:
+            </span>
+            <span className="text-toptext2 text-sm  font-medium text-opacity-70">
+              info@kidstick.com.bd
+            </span>
+          </li>
+          <li className="flex items-center ">
+            <FaPhoneAlt  className="text-white me-[4.57px] sm:text-[15.71px] text-[12px]" />
+            <span className="text-toptext text-xs font-bold me-[5px] sm:block hidden">
+              Call Us:
+            </span>
+            <span className="text-toptext2 text-sm  font-medium text-opacity-70">
+              +8801404049797
+            </span>
+          </li>
+        </ul>
+        <div className="flex lg:gap-[26px] sm:gap-[10px] gap-[20px] items-center">
+          <p className="text-toptext3 text-opacity-70 sm:text-sm text-xs">
+            Visit us on social networks
+          </p>
+          <ul className="list-none flex  items-center gap-[6px] ">
+            <li className="h-[24px] w-[24px] bg-socialcolor1 flex items-center justify-center rounded-full">
+              <a href="https://www.facebook.com/kidslandweb">
+                <FaFacebookF  className="text-white sm:text-base text-xs" />
+              </a>
+            </li>
+            <li className="h-[24px] w-[24px] bg-socialcolor2 flex items-center justify-center rounded-full">
+              <a href="https://www.facebook.com/kidslandweb">
+                <FaInstagram  className="text-white sm:text-base text-xs" />
+              </a>
+            </li>
+            <li className="h-[24px] w-[24px] bg-socialcolor3 flex items-center justify-center rounded-full">
+              <a href="https://www.facebook.com/kidslandweb">
+                <FaTwitter  className="text-white sm:text-base text-xs" />
+              </a>
+            </li>
+            <li className="h-[24px] w-[24px] bg-primary flex items-center justify-center rounded-full">
+              <a href="https://www.facebook.com/kidslandweb">
+                <FaLinkedinIn  className="text-white sm:text-base text-xs" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Topbar;
